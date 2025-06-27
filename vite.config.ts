b50c62bd45a6f8142b8cb7f1d39d4683b3e41e1d
Khaled Ajaj <kh199697@gmail.com>
2025-06-27 18:30:45 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables that start with VITE_
+  const env = loadEnv(mode, process.cwd());
+
+  // Map the VITE_* variables to keys without the prefix.
+  const processEnv = Object.keys(env)
+    .filter((key) => key.startsWith("VITE_"))
+    .reduce((acc, key) => {
+      // Remove the "VITE_" prefix and expose the variable
+      const newKey = key.replace(/^VITE_/, "");
+      acc[`process.env.${newKey}`] = JSON.stringify(env[key]);
+      return acc;
+    }, {} as Record<string, string>);
+
+  return {
+    plugins: [react()],
+    define: processEnv,
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  };
+})
